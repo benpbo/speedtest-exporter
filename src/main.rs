c5964d871ed101951e3c60bd6909c8fde1d67715
Ben Prize Bar-On <prizeben@gmail.com>
2025-03-21 23:33:29 +0100

    Parse response json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use response::Response;
+
+mod response;
+
 fn main() {
-    println!("Hello, world!");
+    let file = std::fs::read_to_string("result.json").unwrap();
+
+    let response: Response = serde_json::from_str(&file).unwrap();
+
+    println!("{response:?}");
 }
---
+++ src/response.rs
@@ -0,0 +1,87 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase", tag = "type")]
+pub enum Response {
+    Result {
+        timestamp: String,
+        ping: Ping,
+        download: Download,
+        upload: Upload,
+        isp: String,
+        interface: Interface,
+        server: Server,
+        result: Result,
+    },
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Ping {
+    pub jitter: f64,
+    pub latency: f64,
+    pub low: f64,
+    pub high: f64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Download {
+    pub bandwidth: i64,
+    pub bytes: i64,
+    pub elapsed: i64,
+    pub latency: Latency,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Latency {
+    pub iqm: f64,
+    pub low: f64,
+    pub high: f64,
+    pub jitter: f64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Upload {
+    pub bandwidth: i64,
+    pub bytes: i64,
+    pub elapsed: i64,
+    pub latency: Latency2,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Latency2 {
+    pub iqm: f64,
+    pub low: f64,
+    pub high: f64,
+    pub jitter: f64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Interface {
+    pub internal_ip: String,
+    pub name: String,
+    pub mac_addr: String,
+    pub is_vpn: bool,
+    pub external_ip: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Server {
+    pub id: i64,
+    pub host: String,
+    pub port: i64,
+    pub name: String,
+    pub location: String,
+    pub country: String,
+    pub ip: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Result {
+    pub id: String,
+    pub url: String,
+    pub persisted: bool,
+}
