9b708e5df1b96ffdd00d97c40225e03ea0559e4f
Ben Prize Bar-On <prizeben@gmail.com>
2025-03-21 23:42:28 +0100

    Read response from process

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use std::process::Command;
+
 use response::Response;
 
 mod response;
 
 fn main() {
-    let file = std::fs::read_to_string("result.json").unwrap();
+    let output = Command::new("speedtest")
+        .arg("--format=json")
+        .arg("--accept-license")
+        .arg("--accept-gdpr")
+        .output()
+        .expect("Failed to execute 'speedtest'");
 
-    let response: Response = serde_json::from_str(&file).unwrap();
+    let response: Response = serde_json::from_slice(&output.stdout).unwrap();
 
     println!("{response:?}");
 }
