ffb4ff22b49f69814d169b080b0278a0753a9ba1
Ben Prize Bar-On <prizeben@gmail.com>
2025-04-17 22:12:47 +0200

    Log errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use core::str;
 use std::{net::SocketAddr, process::Command};
 
-use log::{debug, info};
+use log::{debug, error, info};
 use prometheus_exporter_base::{
     prelude::{Authorization, ServerOptions},
     render_prometheus,
@@ -10,21 +11,22 @@ use response::Response;
 mod render;
 mod response;
 
-fn perform_request() -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
+fn perform_request() -> Result<String, RequestError> {
     info!("Running speedtest");
-    let output = Command::new("speedtest")
+    let stdout = Command::new("speedtest")
         .arg("--format=json")
         .arg("--accept-license")
         .arg("--accept-gdpr")
         .output()
-        .expect("Failed to execute 'speedtest'");
+        .map_err(RequestError::Command)?
+        .stdout;
 
     debug!("Parsing response");
-    let response: Response = serde_json::from_slice(&output.stdout).unwrap();
+    let response = serde_json::from_slice::<Response>(&stdout)
+        .map_err(|error| RequestError::Parse(error, stdout))?;
     debug!("Got {response:?}");
-    let rendered = render::render(response);
-
-    Ok(rendered)
+    let rendred = render::render(response);
+    Ok(rendred)
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -38,7 +40,40 @@ async fn main() {
     };
 
     render_prometheus(server_options, (), |_request, _options| async {
-        perform_request()
+        match perform_request() {
+            Ok(result) => Ok(result),
+            Err(error) => {
+                error!("{error}");
+                Err(Box::new(error).into())
+            }
+        }
     })
     .await;
+}
+
+#[derive(Debug)]
+enum RequestError {
+    Command(std::io::Error),
+    Parse(serde_json::error::Error, Vec<u8>),
+}
+
+impl std::fmt::Display for RequestError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Command(inner) => write!(f, "Failed to execute speedtest command due to {inner}"),
+            Self::Parse(inner, bytes) => match str::from_utf8(&bytes) {
+                Ok(s) => write!(f, "Failed to parse command output: {s} {inner}"),
+                Err(_) => write!(f, "Failed to parse command output: {bytes:#?} {inner}"),
+            },
+        }
+    }
+}
+
+impl std::error::Error for RequestError {
+    fn description(&self) -> &str {
+        match self {
+            Self::Command(_) => "Failed to execute speedtest command",
+            Self::Parse(_, _) => "Failed to parse command output",
+        }
+    }
 }
